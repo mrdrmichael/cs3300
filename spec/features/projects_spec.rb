31025e744eea7e232693a8b1c3d34c2dda0762a0
root <mrdrmichael92@gmail.com>
2022-11-20 04:07:28 +0000

    Tests finally pass

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.feature "Projects", type: :feature do
+  context "New project" do
+    before(:each) do
+      visit new_project_path
+      within("form") do
+        fill_in "Title", with: "Test title"
+      end
+    end
+
+    scenario "should be successful" do
+      fill_in "Description", with: "Test description"
+      click_button "Create Project"
+      expect(page).to have_content("Project was successfully created")
+    end
+
+    scenario "should fail" do
+      click_button "Create Project"
+      expect(page).to have_content("Description can't be blank")
+    end
+  end
+
+  context "Edit this project" do
+    let(:project) { Project.create(title: "Test title", description: "Test content") }
+    before(:each) do
+      visit edit_project_path(project)
+    end
+
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Description", with: "New description content"
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Project was successfully updated")
+    end
+
+    scenario "should fail" do
+      within("form") do
+        fill_in "Description", with: ""
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Description can't be blank")
+    end
+  end
+
+  context "Destroy this project" do
+    let!(:project) { Project.create(title: "Test title", description: "Test content") } 
+    scenario "Destroy this project" do
+      visit project_path(project)
+      click_button "Destroy this project"
+      expect(page).to have_content("Project was successfully destroyed")
+      expect(Project.count).to eq(2)
+    end
+  end
+end
